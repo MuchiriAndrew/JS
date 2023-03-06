d5f033de4980a46c4c921348ceeadafa75994946
MuchiriAndrew <kariukia225@gmail.com>
2023-03-06 08:23:42 +0300

    different function types

--- functions.js
+++ functions.js
@@ -1,6 +1,22 @@
-function testExample() {
+/*function testExample(a) {
+
+    //the 'a' acts as a place holder
+    var greeting = "Hi my name is " + a;
+    return greeting;
+}
+ 
+console.log(testExample("Andrew"));*/
+
+
+/*var testExample = function() {
+    var greeting = "Hi my name is" + a;
+    return greeting;
+}
+//anonymous function
+var a= "Andrew"
+console.log(testExample(a));*/
+
+(function() {
     var greeting = "Hi my name is Andrew";
     console.log(greeting);
-}
-
-testExample();
+}())
