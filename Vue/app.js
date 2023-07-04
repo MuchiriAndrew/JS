15c32b1b02561c47ec527fa200dc5895c1ec8a26
MuchiriAndrew <kariukia225@gmail.com>
2023-07-04 11:54:27 +0300

    update

--- Vue/app.js
+++ Vue/app.js
@@ -5,9 +5,9 @@ const app = Vue.createApp({
             url:"http://www.thentninja.co.uk",
 
             books : [
-                {title: 'name of the wind', author: 'patrick rothfuss', image:"assets/1.jpg", isFav:true},
-                {title: 'the way of kings', author: 'brandon sanderson', image:'assets/2.jpg', isFav:false},
-                {title: 'the final empire', author: 'Andrew Muchiri', image:"assets/3.jpg", isFav:true},
+                {id:1, title: 'name of the wind', author: 'patrick rothfuss', image:"assets/1.jpg", isFav:true},
+                {id:2, title: 'the way of kings', author: 'brandon sanderson', image:'assets/2.jpg', isFav:false},
+                {id:3, title: 'the final empire', author: 'Andrew Muchiri', image:"assets/3.jpg", isFav:true},
             ]
         }
     },
@@ -16,24 +16,18 @@ const app = Vue.createApp({
         toggleShowBooks() {
             this.showBooks = !this.showBooks
         },
-        handleEvent(e, data) {
-            console.log(e, e.type);
 
-            if(data) {
-                console.log(data);
-            }
-        },
-        handleMousemove(e) {
-            this.x = e.offsetX
-            this.y = e.offsetY
-        },
-        toggleFav(item) {
-            item.isFav = !item.isFav
+        toggleFav(book) {
+            book.isFav = !book.isFav
         }
-
-
-
-    }
+    },
+    
+        
+        computed: {
+            filteredBooks() {
+                return this.books.filter((book)=> book.isFav )
+            }
+        }
 })
 
 app.mount('#app')
