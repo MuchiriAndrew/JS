73e97b12c9983b9cb98c9b7c93b025e11e20d3bc
MuchiriAndrew <kariukia225@gmail.com>
2023-07-04 11:00:43 +0300

    attribute binding

--- Vue/app.js
+++ Vue/app.js
@@ -2,16 +2,12 @@ const app = Vue.createApp({
     data(){
         return{
             showBooks:true,
-            // title:"The Final Empire",
-            // author: "Brandon",
-            // age:"45",
-            // x:0,
-            // y:0
+            url:"http://www.thentninja.co.uk",
 
             books : [
-                {title: 'name of the wind', author: 'patrick rothfuss'},
-                {title: 'the way of kings', author: 'brandon sanderson'},
-                {title: 'the final empire', author: 'Andrew Muchiri'},
+                {title: 'name of the wind', author: 'patrick rothfuss', image:"assets/1.jpg"},
+                {title: 'the way of kings', author: 'brandon sanderson', image:'assets/2.jpg'},
+                {title: 'the final empire', author: 'Andrew Muchiri', image:"assets/3.jpg"},
             ]
         }
     },
