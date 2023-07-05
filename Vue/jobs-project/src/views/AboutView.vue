c521d083374d3265b300bdc97236945236136a52
MuchiriAndrew <kariukia225@gmail.com>
2023-07-05 13:15:53 +0300

    routing

--- Vue/jobs-project/src/views/AboutView.vue
+++ Vue/jobs-project/src/views/AboutView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Est perferendis rerum rem voluptate repellat obcaecati enim beatae cupiditate mollitia deserunt, maxime molestias omnis ut provident doloremque. Porro harum culpa corporis reprehenderit quod nihil assumenda, quae doloremque hic rerum possimus consectetur pariatur obcaecati sequi minus autem dolor consequatur praesentium ex impedit quis in. Dignissimos quisquam non atque debitis perspiciatis. Possimus nihil expedita magni beatae dolores, omnis in ullam vitae recusandae. Distinctio ratione quae officiis voluptas, tenetur quaerat. Molestias libero harum quod cum voluptatem repellendus, illo deleniti commodi possimus quo incidunt alias aliquam ut dignissimos voluptate soluta voluptatibus corrupti neque nam ratione!</p>
   </div>
 </template>
--- Vue/jobs-project/src/views/HomeView.vue
+++ Vue/jobs-project/src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt neque veritatis ex assumenda quidem odit et eligendi pariatur id dolores similique doloribus minima autem illum beatae ea ab quia quos quas, soluta voluptate quisquam deleniti. Ipsam tempore dignissimos libero eius sint cum doloribus, dolores vel iste nemo eligendi cupiditate vitae velit, voluptatibus sit quasi enim eum. Provident placeat voluptates, nobis unde deleniti veniam illo ratione voluptatem recusandae libero vero harum ipsa numquam excepturi sequi labore consequuntur ex necessitatibus fugiat doloremque eligendi facere culpa ab. Atque doloremque dicta nulla placeat, laboriosam inventore facilis laborum ipsam, hic, debitis modi est iusto fuga.</p>
+
+     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Deserunt neque veritatis ex assumenda quidem odit et eligendi pariatur id dolores similique doloribus minima autem illum beatae ea ab quia quos quas, soluta voluptate quisquam deleniti. Ipsam tempore dignissimos libero eius sint cum doloribus, dolores vel iste nemo eligendi cupiditate vitae velit, voluptatibus sit quasi enim eum. Provident placeat voluptates, nobis unde deleniti veniam illo ratione voluptatem recusandae libero vero harum ipsa numquam excepturi sequi labore consequuntur ex necessitatibus fugiat doloremque eligendi facere culpa ab. Atque doloremque dicta nulla placeat, laboriosam inventore facilis laborum ipsam, hic, debitis modi est iusto fuga.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
