0ef77e5bb300e562ded7dadd7d4e2030ebd11196
MuchiriAndrew <kariukia225@gmail.com>
2023-07-05 15:11:30 +0300

    update

--- Vue/blog-project/src/views/HomeView.vue
+++ Vue/blog-project/src/views/HomeView.vue
@@ -1,18 +1,40 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>HomePage</h1>
+
+    <input type="text" v-model="search">
+
+    <p>Search term - {{search}}</p>
+
+    <div v-for="name in matchingNames" :key="name">
+      {{ name }}
+    </div>
+    
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref, reactive, computed, watch, watchEffect } from "vue"
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  setup() {
+    const search = ref('')
+    const names = ref(['mario', 'yoshi', 'drew', 'muchiri', 'brandon', 'ashley'])
+
+    watch(search, ()=> {
+      console.log('watch func runs');
+    })
+
+    watchEffect(()=> {
+      console.log('watch effect runs', search.value);
+    })
+
+    const matchingNames = computed(()=> {
+      return names.value.filter((name) => name.includes(search.value))
+    })
+
+    return{ names, search, matchingNames }
   }
 }
 </script>
